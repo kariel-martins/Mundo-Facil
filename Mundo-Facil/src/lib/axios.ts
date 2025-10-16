0297d1e654d85f80ecd78433bbf1ca160a0c50da
kariel <karielemanoel.17@gmail.com>
2025-10-15 22:05:14 -0300

    refactor: add variavel urlBackend para axios

--- Mundo-Facil/src/lib/axios.ts
+++ Mundo-Facil/src/lib/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios"
+
+const urlBackend = import.meta.env.VITE_STRIPE_PUBLIC_KEY as string
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5050/api',
+    baseURL: urlBackend,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
